c2d4dc1f525271b56baf5818323ef9d326988f90
adrian-babilinski <ababilinski@gmail.com>
2020-09-01 16:04:03 -0400

    fix: text

---
+++ Assets/Engine Demo/Scripts/SceneLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public SceneField[] Scenes;
+    
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        var currentScene = SceneManager.GetActiveScene();
+        foreach (SceneField sceneField in Scenes)
+        {
+            if(!SceneManager.GetSceneByName(sceneField).isLoaded)
+            {
+                yield return  SceneManager.LoadSceneAsync(sceneField, LoadSceneMode.Additive);
+            }
+        }
+
+        SceneManager.SetActiveScene(currentScene);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
